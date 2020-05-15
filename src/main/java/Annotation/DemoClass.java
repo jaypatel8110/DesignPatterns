3fb32f736cff64d91b028d70141f7f91f5fee76a
jaypatel8110 <California@2858>
2020-05-14 22:04:26 -0400

    Annotation Class

---
+++ src/main/java/Annotation/DemoClass.java
@@ -0,0 +1,12 @@
+package Annotation;
+
+@JavaFileInfo
+public class DemoClass {
+
+    @JavaFileInfo(author = "jay", version = "1.0")
+    public String getString()
+    {
+        return null;
+    }
+
+}
